5f951fd05a53b3b3dcb4dfa149d5ff3b1dc7c51f
Patrick Lamber <patrick@nubo.eu>
2020-10-24 08:24:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -l
 
 echo "Processing..."
-echo "INPUT_PROFILE"
-echo "INPUT_PROFILE" > input.xml
-echo input.xml
+echo "$INPUT_PROFILE"
+echo "$INPUT_PROFILE" > input.xml
 ftpURL=$(xmllint --xpath 'string((//publishData/publishProfile[@publishMethod = "FTP"])[1]/@publishUrl)' input.xml)
 userName=$(xmllint --xpath 'string((//publishData/publishProfile[@publishMethod = "FTP"])[1]/@userName)' input.xml)
 userPWD=$(xmllint --xpath 'string((//publishData/publishProfile[@publishMethod = "FTP"])[1]/@userPWD)' input.xml)
 
-echo "Connecting to remote host and deleting files..."
+echo "Connecting to remote host $ftpURL using $userName and deleting files..."
 lftp $ftpURL -u $userName,$userPWD -e "set ftp:ssl-allow no; cd \"$INPUT_WORKINGDIR\"; rm -r .; quit;"
